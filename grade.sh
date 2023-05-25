94ea929849f2878b698395f99d82aca5c9290f7d
XIC051 <xic051@ucsd.edu>
2023-05-25 10:57:06 -0700

    updating to github

--- grade.sh
+++ grade.sh
@@ -8,7 +8,30 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+if [[ ! -e student-submission/ListExamples.java ]]
+then
+    echo "Error: ListExamples.java not found in submission directory"
+    exit 1
+fi
 
+cp student-submission/ListExamples.java grading-area/
+cp TestListExamples.java grading-area/
+cp -r lib grading-area/
+cd grading-area/
+
+javac -cp $CPATH *.java
+# java -cp $CPATH TestListExamples
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
+
+# java TestListExamples > out2.txt 2>&1
+# cat out2.txt
+# if [[ $? -ne 0 ]]
+# then
+#     echo  "Error: Compilation failed"
+#     exit 1
+# fi
+# java TestListExamples.java >out.txt 2>&1
+# cat out.txt
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
